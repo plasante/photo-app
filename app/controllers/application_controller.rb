4251f5de55f8b262f7d95269d23b600fd9300d45
Pierre Lasante <pierre.lasante@gmail.com>
2017-07-13 15:04:53 +0000

    Add devise and styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # You need to be a logged in user to access
+  before_action :authenticate_user!
 end
