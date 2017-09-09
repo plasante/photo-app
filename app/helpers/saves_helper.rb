dd1bc86b4fc11fe3d005cad249204473bb4365fc
Pierre Lasante <pierre.lasante@videotron.ca>
2017-09-09 09:24:18 -0400

    Change the jumbotron color

--- app/helpers/saves_helper.rb
+++ app/helpers/saves_helper.rb
@@ -20,6 +20,7 @@ module SavesHelper
   def findBestPriceBySubCategory(store, category, store_array, category_idx, store_idx)
     product = extractBestProductPriceByDescription(store, category)
     puts "\t\tBest price for #{category.category.name}->#{product.description.description}->#{product.provider.name} is in store : #{store.name} at #{product.price}" 
+    store_array[store_idx][0] = product.store.id
     store_array[store_idx][category_idx] = product.price.to_f / 100
   end
   
@@ -30,7 +31,7 @@ module SavesHelper
     id = %q["myTable"]
     table = %Q[<table class="table table-hover" id=#{id}>]
     table += %Q[<thead class=thead-inverse><tr><th></th><th>#{best_price_link}</th><th>Maxi</th><th>Metro Plus</th></tr></thead><tbody>]
-    for i in 0..category_array.size
+    for i in 1..category_array.size
       if (i < category_array.size)
         categorie = category_array[i]
         categorie_translate = I18n.t(categorie)
@@ -62,15 +63,14 @@ module SavesHelper
     num_of_categories = categories.count
     #puts "Number of categories = #{num_of_categories}"
     
-    category_idx = 0
+    category_idx = 1
     store_idx = 0
     category_array = Array.new(categories.size)
-    store_array = Array.new(num_of_stores) { Array.new(categories.size + 1, 0) }
+    store_array = Array.new(num_of_stores) { Array.new(categories.size + 2, 0) }
     
     stores = Store.all
     categories.each do |category|
       category_array[category_idx] = category.category.name
-      #puts "Category = " + category.category.name
       store_idx = 0
       stores.each do |store|
         #findBestPriceByCategory(store, category, store_array, category_idx, store_idx)
@@ -84,14 +84,28 @@ module SavesHelper
     p category_array
     
     # Calculating the sum for each store
-    store_array.each do |row|
-      row[-1] = row.inject(0, :+)
+#    store_array.each do |row|
+#      row[-1] = row.inject(0, :+)
+#    end
+    
+    i = 0
+    j = 1
+    until i >= 2
+      sum = 0
+      until j >= category_array.length - 1
+        sum += store_array[i][j]
+        j = j + 1
+      end
+      store_array[i][-1] = sum
+      i = i + 1
     end
+    
     # Sorting by the last element ascending order
     store_array.sort_by!(&:last)
-    #pp store_array
-    
-    best_store = Store.find(2)
+
+    # I need to find the best store.id
+    #best_store = Store.find(2)
+    best_store = Store.find(store_array[0][0])
     
     html = build_html_table( category_array , store_array , best_store).html_safe
   end
